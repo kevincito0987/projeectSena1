31e354d748b01eab7af304a0fdcb2b7035ce72f0
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-17 14:38:07 -0500

    perf: :sparkles: I've created structure of this component.

--- components/bgImageContainer.js
+++ components/bgImageContainer.js
@@ -1,1 +1,13 @@
 console.log("Funciona bgImageContainer.js");
+
+function cambiarImagenDeFondo(nuevaURL) {
+    const contenedorImagen = document.querySelector('.image-container');
+    if (contenedorImagen) {
+      contenedorImagen.style.backgroundImage = `url('${nuevaURL}')`;
+    } else {
+      console.error('No se encontró ningún elemento con la clase "image-container".');
+    }
+  }
+  
+  // Ejemplo de cómo usar la función para cambiar la imagen:
+  cambiarImagenDeFondo('../icons/icon5LogSingUpPage.svg');
